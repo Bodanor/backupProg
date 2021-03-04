9f85083ac9a161995f1c4f72cff1e57e2a4d8e47
christos <papadopoulos.chris@icloud.com>
2021-03-04 21:53:32 +0100

    Fixed some error and added some error messages

--- Receiver.py
+++ Receiver.py
@@ -22,55 +22,64 @@ print(f"[{time.time() - start_server_timer}]Server receiver Started !")
 
 
 def client_handler(conn):
-    global dest_folder
+    try:
+        global dest_folder
 
-    folder_info = datetime.datetime.now()
-    dt_string = folder_info.strftime("_%d%m%y_%H_%M_%S")
-    dest_folder_local = dest_folder + dt_string + "/"
+        folder_info = datetime.datetime.now()
+        dt_string = folder_info.strftime("_%d%m%y_%H_%M_%S")
+        dest_folder_local = dest_folder + dt_string + "/"
 
-    if not os.path.exists(dest_folder_local):
-        os.mkdir(dest_folder_local)
-    starting_connection_timer = time.time()
+        if not os.path.exists(dest_folder_local):
+            os.mkdir(dest_folder_local)
+        starting_connection_timer = time.time()
 
-    while True:
+        while True:
 
-        file_name_header = conn.recv(HEADERSIZE)
-        if not (file_name_header):
-            print("All files received !")
-            print(f"Collected all files in {time.time() - starting_connection_timer} seconds")
-            break
+            file_name_header = conn.recv(HEADERSIZE)
+            if not (file_name_header):
+                print("All files received !")
+                print(f"Collected all files in {time.time() - starting_connection_timer} seconds")
+                break
 
-        file_name_length = int(file_name_header.decode().strip())
-        file_name = conn.recv(file_name_length).decode()
-        file_size_header = conn.recv(HEADERSIZE)
+            file_name_length = int(file_name_header.decode().strip())
+            file_name = conn.recv(file_name_length).decode()
+            file_size_header = conn.recv(HEADERSIZE)
 
-        if not (file_size_header):
-            print("Something went wrong !")
-            break
-        file_size_length = int(file_size_header.decode().strip())
-        file_size = int(conn.recv(file_size_length).decode())
+            if not (file_size_header):
+                print("Something went wrong !")
+                conn.close()
+                break
+            file_size_length = int(file_size_header.decode().strip())
+            file_size = int(conn.recv(file_size_length).decode())
 
 
-        file_name = dest_folder_local + file_name
-        if not os.path.exists(os.path.dirname(file_name)):
-            os.makedirs(os.path.dirname(file_name))
+            file_name = dest_folder_local + file_name
+            if not os.path.exists(os.path.dirname(file_name)):
+                os.makedirs(os.path.dirname(file_name))
 
-        file_transfer_timer = time.time()
-        with open(file_name, "wb") as file:
-            count = 0
+            file_transfer_timer = time.time()
+            with open(file_name, "rb") as file:
+                count = 0
 
-            while count < file_size:
-                data = conn.recv(file_size)
-                if not data:
-                    break
-                file.write(data)
+                while count < file_size:
+                    data = conn.recv(file_size)
+                    if not data:
+                        break
+                    file.write(data)
 
-                count += len(data)
+                    count += len(data)
 
-        status = "OK".encode()
+            status = "OK".encode()
+            status_header = f"{len(status) : < {HEADERSIZE}}".encode()
+            conn.sendall(status_header + status)
+            print(f"File \"{file_name}\" received in {time.time() - file_transfer_timer}")
+
+    except FileNotFoundError:
+        status = "ERROR".encode()
         status_header = f"{len(status) : < {HEADERSIZE}}".encode()
         conn.sendall(status_header + status)
-        print(f"File \"{file_name}\" received in {time.time() - file_transfer_timer}")
+        conn.close()
+
 
 
 while True:
